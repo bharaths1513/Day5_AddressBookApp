54372c36f437fb706f6e130b752bef870ea9012c
bharaths1513 <bharaths.1311@gmail.com>
2021-12-24 21:44:26 +0530

    added: Section 2 and UC3 Storing Data in List

--- src/main/java/com/example/Demo/Service/AddressBookService.java
+++ src/main/java/com/example/Demo/Service/AddressBookService.java
@@ -11,35 +11,49 @@ import com.example.Demo.model.AddressBookData;
 public class AddressBookService implements IAddressBookService {
 
 
+
+	private List<AddressBookData> addresslist = new ArrayList<>();
+
 	@Override
 	public AddressBookData AddEmployeeData(AddressBookData addressdata) {
-		
+		addresslist.add(addressdata);
 		return addressdata;
 	}
 
 	@Override
-	public AddressBookData FindEmployeeData(int Slno) {
-		AddressBookData addressdata =null;
-		addressdata = new AddressBookData(1,"Bharath S","Channagiri",577213);
-		return addressdata;
+	public AddressBookData FindEmployeeData(int empId) {
+		return addresslist.get(empId - 1);
 	}
 
 	@Override
 	public List<AddressBookData> FindData() {
-		List<AddressBookData> addresslist = new ArrayList<AddressBookData>();
-		addresslist.add(new AddressBookData());
 		return addresslist;
 	}
 
 	@Override
-	public AddressBookData updateEmployeeData(AddressBookData addressdata) {
-		addressdata = new AddressBookData(1,"Bharath","Davanagere",577215);
-		return addressdata;
+	public AddressBookData updateEmployeeData(AddressBookData empdata) {
+		addresslist.set(1, empdata);
+		return empdata;
 	}
 
+//	@Override
+//	public EmployeePayrollData updateEmployeeData( EmployeePayrollData empdata) {
+//		int empId=0;
+//		empdata.setEmpId(empId);
+//		String name = "";
+//		empdata=empdata.setEmpName(name);
+//		long salary = 0;
+//		empdata=empdata.setEmpSalary(salary);
+//		
+//		emplist.set(empId-1,empdata);
+//		return empdata;
+//	}
+
 	@Override
-	public void DeleteData(int Slno) {
-		
-		
+	public void DeleteData(int empId) {
+		addresslist.remove(empId - 1);
+
 	}
+
+	
 }
